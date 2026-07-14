cedca20d8ca3d5eab4c90d40a16b3d2ee7643947
root <atilalima569@gmail.com>
2026-07-14 17:36:04 -0500

    masscan

--- masscan.sh
+++ masscan.sh
@@ -1,8 +1,14 @@
-
-
 #script para abaixa e   gera os ips  com o masscan
 
 
-
-
 curl  -L   https://github.com/atilabyte/IA/raw/refs/heads/main/masscan  -o  /tmp/masscan
+
+
+port=22
+
+
+
+
+
+timeout   5  ./masscan  -p $port  188.166.107.167/16   >      /tmp/ips.txt  
+ 
